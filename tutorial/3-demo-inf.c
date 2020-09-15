3efe4fedada15be1d5bcc600559222ec93739afa
Athesto <gamez.live@gmail.com>
2020-09-15 02:30:42 +0000

    3-demo-inf.c

---
+++ tutorial/3-demo-inf.c
@@ -0,0 +1,20 @@
+#include "demo.h"
+
+int main(void)
+{
+	SDL_Instance instance;
+	if (init_instance(&instance) != 0)
+		return(1);
+
+	while ("C is awesome")
+	{
+		SDL_SetRenderDrawColor(instance.renderer, 0, 0, 0, 0);
+		SDL_RenderClear(instance.renderer);
+		/*
+		 * awesome things here \o/
+		 */
+		SDL_RenderPresent(instance.renderer);
+	}
+
+	return(0);
+}
