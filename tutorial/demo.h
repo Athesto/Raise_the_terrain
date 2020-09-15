db5d4cfc032eb9ebceb81aaa4ce1dd094a727045
Athesto <gamez.live@gmail.com>
2020-09-15 01:22:07 +0000

    installing sript
    demo1 & makefile

---
+++ tutorial/demo.h
@@ -0,0 +1,12 @@
+#ifndef _DEMO_H_
+#define _DEMO_H_
+#include <SDL2/SDL.H>
+
+typedef struct SDL_Instance
+{
+	SDL_Window *window;
+	SDL_Renderer *renderer;
+} SDL_Instance;
+
+int init_instance(SDL_Instance *);
+#endif /* _DEMO_H_ */
