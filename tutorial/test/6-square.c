661f3fb562a323494ad844aa0636a328b5ae5e4f
Athesto <gamez.live@gmail.com>
2020-09-20 21:10:50 -0500

    change low border parameter

--- tutorial/test/6-square.c
+++ tutorial/test/6-square.c
@@ -70,7 +70,7 @@ void draw_squarerow(SDL_Instance instance, int m_len)
 		cell.x +=cell.len;
 	}
 
-	SDL_RenderDrawLine(instance.renderer, o.x, o.y + o.len, pend.x, pend.y); /* x */
+	SDL_RenderDrawLine(instance.renderer, o.x, pend.y, pend.x, pend.y); /* x */
 	SDL_RenderDrawLine(instance.renderer, pend.x, o.y, pend.x, pend.y); /* y */
 
 
